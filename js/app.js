6c1ca04e9fa27471b49e3e600b043718cf9c2ea1
rcdavis4 <rcdavis4@gmail.com>
2015-02-12 11:59:31 -0800

    added break

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   for (var index = 1; index <= 100; index++) {
     if (index % 3 == 0 && index % 5 == 0) {
-      $('body').append('FIZZBUZZ <br>');
+      $('body').append('FIZZBUZZ <br><br>');
     }
     else if (index % 3 == 0) {
-      $('body').append('fizz <br>');
+      $('body').append('fizz <br><br>');
     }
     else if (index % 5 == 0) {
-      $('body').append('buzz <br>');
+      $('body').append('buzz <br><br>');
     }
     else {
-      $('body').append(index + '<br>');
+      $('body').append(index + '<br><br>');
     }
   }
 }); // end of document.ready
