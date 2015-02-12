d2152fb7f239b9c7ac3945eb20e31c80e9c7bcf6
rcdavis4 <rcdavis4@gmail.com>
2015-02-12 11:55:46 -0800

    finished plain logic

--- js/app.js
+++ js/app.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
-
-
+  for (var index = 1; index <= 100; index++) {
+    if (index % 3 == 0 && index % 5 == 0) {
+      $('body').append('FIZZBUZZ <br>');
+    }
+    else if (index % 3 == 0) {
+      $('body').append('fizz <br>');
+    }
+    else if (index % 5 == 0) {
+      $('body').append('buzz <br>');
+    }
+    else {
+      $('body').append(index + '<br>');
+    }
+  }
 }); // end of document.ready
+
+
+
+
+
+
+
+// $('#number-list').append('<li class="numbers">' + index);
