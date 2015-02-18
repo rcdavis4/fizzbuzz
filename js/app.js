e97cacb86bfd76c40eb9f02960ed2c289809dd65
rcdavis4 <rcdavis4@gmail.com>
2015-02-18 15:51:31 -0800

    finished functions

--- js/app.js
+++ js/app.js
@@ -1,61 +1,31 @@
-//$(document).ready(function() {
-//
-//  var debug = function DEBUG(msg) {
-//    console.log(msg);
-//  };
-//
-//  var fizzCount = 0, buzzCount = 0, fizzBuzzCount = 0;
-//
-//  for (var index = 1; index <= 100; index++) {
-//    if (index % 3 === 0 && index % 5 === 0) {
-//      $('body').append('FIZZBUZZ <br><br>');
-//      fizzBuzzCount++;
-//    }
-//    else if (index % 3 === 0) {
-//      $('body').append('fizz <br><br>');
-//      fizzCount++;
-//    }
-//    else if (index % 5 === 0) {
-//      $('body').append('buzz <br><br>');
-//      buzzCount++;
-//    }
-//    else {
-//      $('body').append(index + '<br><br>');
-//    }
-//  }
-//    debug('fizz count: ' + fizzCount);
-//    debug('buzzCount: ' + buzzCount);
-//    debug('fizz buzz count: ' + fizzBuzzCount);
-//
-//}); // end of document.ready
+var userInput = prompt("Enter A Number"); // gets user input
+var userNumber = Number(userInput); // converts input string into a number
 
-
-// $('#number-list').append('<li class="numbers">' + index);
-
-
-
-
-
-/* ________________________________________________________________*/
-
-// get user number
-var userInput = prompt("Enter A Number");
-
-
-function isValid(userIn) {
-  var userNumber = Number(userIn);  // converts input string into number
-  // var userNumber = +userInput; // alternative to converting input string into number
-
-  // test for input: 1) being greater than '0' 2) being a number 3) being a whole number
-  while ((userNumber < 1) || (isNaN) || (userNumber % 1 != 0)) {
-    return prompt("Enter a VALID whole number");
+// fizzbuzz function, takes in number as parameter.
+function fizzBuzz(number) {
+  // validates number: 1) greater than zero  2) whole number  3) is a number
+  if ((number < 1) || (number % 1 != 0) || isNaN(number)) {
+    alert('Invalid Entry!');
   }
   else {
-    return userNumber;
-  }
-}
+    // loops from 1 up to user's number and tests for fizzbuzz output
+    for (var index = 1; index <= num; index++) {
+      if (index % 3 === 0 && index % 5 === 0) {
+        $('body').append('FIZZBUZZ <br><br>');
+      }
+      else if (index % 3 === 0) {
+        $('body').append('FIZZ <br><br>');
+      }
+      else if (index % 5 === 0) {
+        $('body').append('BUZZ <br><br>');
+      }
+      else {
+        $('body').append(index + '<br><br>');
+      }
+    } // end for loop
+  } // end outter else
+} // end fizzbuzz function
 
 
-isValid(userInput);
-
-//function fizzBuzz(validNumber);
+// function call
+fizzBuzz(userNumber);
